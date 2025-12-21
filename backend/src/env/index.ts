cdbc3dc22792b04aac112be5ac9d438a07dd8aaf
Vitor Levi <levivitor8@gmail.com>
2025-12-21 00:42:25 -0300

    add env

--- backend/src/env/index.ts
+++ backend/src/env/index.ts
@@ -1,18 +1,22 @@
-import "dotenv/config"
-import z from "zod"
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().optional(),
+  DATABASE_URL: z.string(),
+  JWT_SECRET: z.string(),
+
+  CLOUDINARY_CLOUD_NAME: z.string(),
+  CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_API_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error("❌ Invalid environment variables", _env.error.format())
-  throw new Error("Invalid environment variables")
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
