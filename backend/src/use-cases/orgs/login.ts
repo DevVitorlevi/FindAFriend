30cd6b3d7ce20f7156c86b41d3004f9f234c43fd
Vitor Levi <levivitor8@gmail.com>
2025-12-20 22:57:33 -0300

    feat(use-case): add login org use case

---
+++ backend/src/use-cases/orgs/login.ts
@@ -0,0 +1,35 @@
+import type { OrgsRepository } from "@/repositories/orgs-interface-repository.js"
+import { InvalidCredentials } from "@/utils/errors/invalid-credentials.js"
+import { compare } from "bcryptjs"
+import type { Org } from "generated/prisma/browser.js"
+
+interface LoginUseCaseRequest {
+  email: string,
+  password: string
+}
+interface LoginUseCaseResponse {
+  org: Org
+}
+
+export class LoginUseCase {
+  constructor(private orgsRepository: OrgsRepository) {
+  }
+
+  async execute({ email, password }: LoginUseCaseRequest): Promise<LoginUseCaseResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentials()
+    }
+
+    const doesPasswordMarches = await compare(password, org.password_hash)
+
+    if (!doesPasswordMarches) {
+      throw new InvalidCredentials()
+    }
+
+    return {
+      org
+    }
+  }
+}
