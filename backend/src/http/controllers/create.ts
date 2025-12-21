31c79596ed6e1b66e5527097fcfd5abbb542ddf5
Vitor Levi <levivitor8@gmail.com>
2025-12-21 19:43:50 -0300

    feat(controller): add controller to create pet

---
+++ backend/src/http/controllers/create.ts
@@ -0,0 +1,36 @@
+import { makeCreatePetUseCase } from "@/use-cases/factories/make-create-pet-use-case.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createPetBodySchema = z.object({
+    name: z.string(),
+    description: z.string().max(300),
+    age: z.enum(['FILHOTE', 'ADULTO', 'IDOSO']),
+    size: z.enum(['PEQUENO', 'MEDIO', 'GRANDE']),
+  })
+
+  const orgIdParamsSchema = z.object({
+    orgId: z.string()
+  })
+
+  const { name, description, age, size } = createPetBodySchema.parse(request.body)
+  const { orgId } = orgIdParamsSchema.parse(request.params)
+  try {
+    const createPetUseCase = makeCreatePetUseCase()
+    const pet = await createPetUseCase.execute({
+      name,
+      description,
+      age,
+      size,
+      orgId
+    })
+    return reply.status(201).send({
+      message: "Pet Created!!",
+      pet
+    })
+  } catch (error) {
+    throw error
+  }
+
+}
