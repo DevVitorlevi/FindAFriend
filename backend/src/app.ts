794d51e342a7bf9ead809b6ef7dca29a068dc416
Vitor Levi <levivitor8@gmail.com>
2025-12-21 00:42:12 -0300

    config cloudinary

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,14 @@
 import fastify from "fastify";
 
 export const app = fastify()
+
+import { env } from '@/env/index.js';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: env.CLOUDINARY_CLOUD_NAME,
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET,
+})
+
+export { cloudinary };
