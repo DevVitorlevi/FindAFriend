4f079704036877683e09a34bae9c9e4429731417
Vitor Levi <levivitor8@gmail.com>
2025-12-21 16:42:46 -0300

    feat(controllers): add register org controller

---
+++ backend/src/http/controllers/register.ts
@@ -0,0 +1,62 @@
+import { makeCreateOrgUseCase } from "@/use-cases/factories/make-create-org-use-case.js";
+import { OrgAlreadyExits } from "@/utils/errors/org-already-exist.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(8),
+    address: z.string(),
+    cep: z
+      .string()
+      .regex(/^\d{5}-?\d{3}$/, 'CEP inválido'),
+    whatsapp: z
+      .string()
+      .transform(value => value.replace(/\D/g, ''))
+      .refine(value => {
+        return /^\d{2}9\d{8}$/.test(value)
+      }, {
+        message: 'Número de WhatsApp inválido',
+      }),
+    latitude: z.number().refine(value => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine(value => {
+      return Math.abs(value) <= 180
+    }),
+    city: z.string(),
+    state: z.string(),
+  })
+
+  const { name, email, password, address, cep, whatsapp, latitude, longitude, city, state } = registerBodySchema.parse(request.body)
+
+  try {
+    const createOrgUseCase = makeCreateOrgUseCase()
+    await createOrgUseCase.execute({
+      name,
+      email,
+      password,
+      address,
+      cep,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      state
+    })
+  } catch (error) {
+    if (error instanceof OrgAlreadyExits) {
+      return reply.status(409).send({
+        message: error.message
+      })
+    }
+
+    throw error
+  }
+
+  return reply.status(201).send({
+    message: "Organization created!!"
+  })
+}
