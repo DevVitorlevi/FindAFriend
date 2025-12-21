684a3b61e7e9446e18768ac1e13aa80cd509efc1
Vitor Levi <levivitor8@gmail.com>
2025-12-21 17:10:14 -0300

    feat(routes):add route to login of orgs and refresh

--- backend/src/http/routes/orgs.routes.ts
+++ backend/src/http/routes/orgs.routes.ts
@@ -1,6 +1,10 @@
 import type { FastifyInstance } from "fastify";
+import { login } from "../controllers/login.js";
+import { refresh } from "../controllers/refresh.js";
 import { register } from "../controllers/register.js";
 
 export function orgsRoutes(app: FastifyInstance) {
   app.post("/orgs", register)
+  app.post("/sessions", login)
+  app.patch("/token/refresh", refresh)
 }
