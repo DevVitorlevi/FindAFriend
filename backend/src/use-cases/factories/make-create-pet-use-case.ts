4bcccd952481d472efd6c71aeb6422ed0604397c
Vitor Levi <levivitor8@gmail.com>
2025-12-21 14:58:24 -0300

    feat(factories): add makeCreatePetUseCase

---
+++ backend/src/use-cases/factories/make-create-pet-use-case.ts
@@ -0,0 +1,9 @@
+
+import { PrismaPetsRepository } from "@/repositories/prisma/prisma-pets-repository.js";
+import { CreatePetUseCase } from "../pets/create.js";
+
+export function makeCreatePetUseCase() {
+  const prismaPetsRepository = new PrismaPetsRepository()
+
+  const createPetUseCase = new CreatePetUseCase(prismaPetsRepository)
+}
