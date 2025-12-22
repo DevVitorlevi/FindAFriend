d2fc3757b649c1ccd8c2ec50c04d25f399861082
Vitor Levi <levivitor8@gmail.com>
2025-12-22 01:11:55 -0300

    feat(routes): add routes to delete pet image

--- backend/src/http/routes/pets.routes.ts
+++ backend/src/http/routes/pets.routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import { adopted } from "../controllers/pets/adopted.js";
 import { create } from "../controllers/pets/create.js";
+import { deleteImage } from "../controllers/pets/delete-image.js";
 import { fetchMany } from "../controllers/pets/fetch-many.js";
 import { getPet } from "../controllers/pets/get-pet.js";
 import { uploadImages } from "../controllers/pets/upload-images.js";
+import { verifyImageOwnership } from "../middlewares/verify-image-ownership.js";
 import { verifyJWT } from "../middlewares/verify-jwt.js";
 import { verifyPetOwnership } from "../middlewares/verify-pet-ownership.js";
 
@@ -13,4 +15,5 @@ export function petsRoutes(app: FastifyInstance) {
   app.get("/pet/:petId", getPet)
   app.post("/pet/:petId/images", { onRequest: [verifyJWT, verifyPetOwnership] }, uploadImages)
   app.patch("/pet/:petId", { onRequest: [verifyJWT, verifyPetOwnership] }, adopted)
+  app.delete("/image/:imageId", { onRequest: [verifyJWT, verifyImageOwnership] }, deleteImage)
 }
