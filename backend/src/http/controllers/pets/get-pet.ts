c39727b3d63ad9252b9823b44d8f9428a392c64f
Vitor Levi <levivitor8@gmail.com>
2025-12-21 21:18:29 -0300

    feat(controller): add controller to get pet

---
+++ backend/src/http/controllers/pets/get-pet.ts
@@ -0,0 +1,22 @@
+import { makeGetPetUseCase } from "@/use-cases/factories/make-get-pet-use-case.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+export async function getPet(request: FastifyRequest, reply: FastifyReply) {
+  const GetPetParamsSchema = z.object({
+    petId: z.string()
+  })
+  const { petId } = GetPetParamsSchema.parse(request.params)
+  try {
+    const getPetUseCase = makeGetPetUseCase()
+    const pet = await getPetUseCase.execute({
+      petId
+    })
+    return reply.status(200).send({
+      pet
+    })
+  } catch (error) {
+    throw error
+  }
+
+}
