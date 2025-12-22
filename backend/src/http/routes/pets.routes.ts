32077d419212c7be66086e672f1979810e95e8a2
Vitor Levi <levivitor8@gmail.com>
2025-12-21 21:00:32 -0300

    feat(routes):add route to find many pets

--- backend/src/http/routes/pets.routes.ts
+++ backend/src/http/routes/pets.routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from "fastify";
-import { create } from "../controllers/create.js";
+import { create } from "../controllers/pets/create.js";
+import { fetchMany } from "../controllers/pets/fetch-many.js";
 import { verifyJWT } from "../middlewares/verify-jwt.js";
 
 export function petsRoutes(app: FastifyInstance) {
   app.post("/pets/:orgId/create", { onRequest: [verifyJWT] }, create)
+  app.get("/pets", fetchMany)
 }
