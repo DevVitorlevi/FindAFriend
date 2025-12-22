050fe1893735c7af6d867812bb038db69ea8bb18
Vitor Levi <levivitor8@gmail.com>
2025-12-21 21:58:15 -0300

    feat(routes):add route to upload images

--- backend/src/http/routes/pets.routes.ts
+++ backend/src/http/routes/pets.routes.ts
@@ -2,10 +2,13 @@ import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/pets/create.js";
 import { fetchMany } from "../controllers/pets/fetch-many.js";
 import { getPet } from "../controllers/pets/get-pet.js";
+import { uploadImages } from "../controllers/pets/upload-images.js";
 import { verifyJWT } from "../middlewares/verify-jwt.js";
+import { verifyPetOwnership } from "../middlewares/verify-pet-ownership.js";
 
 export function petsRoutes(app: FastifyInstance) {
   app.post("/pets/:orgId/create", { onRequest: [verifyJWT] }, create)
   app.get("/pets", fetchMany)
   app.get("/pet/:petId", getPet)
+  app.post("/pet/:petId/images", { onRequest: [verifyJWT, verifyPetOwnership] }, uploadImages)
 }
