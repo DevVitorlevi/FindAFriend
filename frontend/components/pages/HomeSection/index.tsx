91d8a01a4fdd0c98f10d7a0895109e91c02b8a56
Vitor Levi <levivitor8@gmail.com>
2025-12-28 03:07:15 -0300

    feat(components): add home section components

---
+++ frontend/components/pages/HomeSection/index.tsx
@@ -0,0 +1,23 @@
+import Pets from "@/public/OBJECTS.png"
+import Image from "next/image"
+export default function HomeSection() {
+
+  return (
+    <>
+      <main className="bg-[#F15156] w-screen h-screen px-50 py-50 flex justify-between">
+        <div className="text-white flex flex-col space-y-20 ">
+          <h2 className="font-semibold text-4xl">FindAFriend</h2>
+          <h1 className="font-bold text-7xl">Leve <br />
+            a felicidade <br />
+            para o seu lar</h1>
+          <p className="font-semibold text-justify text-3xl" >Encontre o animal de estimação ideal <br />
+            para seu estilo de vida!</p>
+        </div>
+
+        <div>
+          <Image src={Pets} alt="Imagens Ilustrativa de Animais" className="mt-5" />
+        </div>
+      </main>
+    </>
+  )
+}
