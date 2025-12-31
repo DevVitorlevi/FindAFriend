72dc15bb47a89bd9379f8c19512dae176e130bdf
Vitor Levi <levivitor8@gmail.com>
2025-12-31 00:50:46 -0300

    feat(validations):add validations to forms

--- frontend/lib/validations/register.ts
+++ frontend/lib/validations/register.ts
@@ -1,28 +1,38 @@
 import z from "zod";
 
 export const registerBodySchema = z.object({
-  name: z.string().min(3,
-    {
-      message: "O Nome Da Organizacao teve ter ao menos 3 Letras"
-    }
-  ),
-  email: z.email({ message: "Email Invalido" }),
-  password: z.string().min(8, {
-    message: "A senha deve ter no ao menos 8 Caracteres"
+  name: z.string().min(3, {
+    message:
+      "O nome da organização deve ter ao menos 3 letras",
   }),
-  address: z.string(),
+
+  email: z.email({
+    message: "Email inválido",
+  }),
+
+  password: z.string().min(8, {
+    message:
+      "A senha deve ter ao menos 8 caracteres",
+  }),
+
   cep: z
     .string()
-    .regex(/^\d{5}-?\d{3}$/, 'CEP inválido'),
+    .regex(/^\d{5}-?\d{3}$/, "CEP inválido"),
+
   whatsapp: z
     .string()
-    .transform(value => value.replace(/\D/g, ''))
-    .refine(value => {
-      return /^\d{2}9\d{8}$/.test(value)
-    }, {
-      message: 'Número de WhatsApp inválido',
-    }),
+    .transform((value) =>
+      value.replace(/\D/g, "")
+    )
+    .refine(
+      (value) =>
+        /^\d{2}9\d{8}$/.test(value),
+      {
+        message:
+          "Número de WhatsApp inválido",
+      }
+    ),
+
   city: z.string(),
   state: z.string(),
-  street: z.string()
-})
+});
