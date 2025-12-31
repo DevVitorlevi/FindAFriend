fa9938dbdb462d1a0e073301e4c1cec140136f63
Vitor Levi <levivitor8@gmail.com>
2025-12-31 00:51:59 -0300

    feat(components): add masks in build forms

--- frontend/components/BuildForms/index.tsx
+++ frontend/components/BuildForms/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   FormControl,
   FormField,
@@ -7,26 +8,40 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+import {
+  FieldValues,
+  Path,
+  UseFormReturn,
+} from "react-hook-form";
 import { StateSelectField } from "./StateSelectField";
 import { CitySelectField } from "./CitySelectField";
 
-export type FormFieldType = "input" | "state-select" | "city-select";
+export type FormFieldType =
+  | "input"
+  | "state-select"
+  | "city-select";
 
-export interface IFormsFields<T extends FieldValues> {
+export interface IFormsFields<
+  T extends FieldValues
+> {
   name: Path<T>;
   label: string;
   placeholder?: string;
   type?: React.HTMLInputTypeAttribute;
   fieldType?: FormFieldType;
+  onChangeValue?: (value: string) => string;
 }
 
-interface BuildFormsProps<T extends FieldValues> {
+interface BuildFormsProps<
+  T extends FieldValues
+> {
   formsList: UseFormReturn<T>;
   formsFields: Array<IFormsFields<T>>;
 }
 
-export default function BuildForms<T extends FieldValues>({
+export default function BuildForms<
+  T extends FieldValues
+>({
   formsList,
   formsFields,
 }: BuildFormsProps<T>) {
@@ -51,7 +66,9 @@ export default function BuildForms<T extends FieldValues>({
                 form={formsList}
                 name={field.name}
                 label={field.label}
-                stateFieldName={"state" as Path<T>}
+                stateFieldName={
+                  "state" as Path<T>
+                }
               />
             );
 
@@ -61,16 +78,44 @@ export default function BuildForms<T extends FieldValues>({
                 key={field.name}
                 control={formsList.control}
                 name={field.name}
-                render={({ field: formField }) => (
+                render={({
+                  field: formField,
+                }) => (
                   <FormItem>
-                    <FormLabel>{field.label}</FormLabel>
+                    <FormLabel>
+                      {field.label}
+                    </FormLabel>
+
                     <FormControl>
                       <Input
                         {...formField}
-                        type={field.type ?? "text"}
-                        placeholder={field.placeholder}
+                        type={
+                          field.type ?? "text"
+                        }
+                        placeholder={
+                          field.placeholder
+                        }
+                        onChange={(e) => {
+                          const value =
+                            e.target.value;
+
+                          if (
+                            field.onChangeValue
+                          ) {
+                            formField.onChange(
+                              field.onChangeValue(
+                                value
+                              )
+                            );
+                          } else {
+                            formField.onChange(
+                              value
+                            );
+                          }
+                        }}
                       />
                     </FormControl>
+
                     <FormMessage />
                   </FormItem>
                 )}
@@ -81,4 +126,3 @@ export default function BuildForms<T extends FieldValues>({
     </>
   );
 }
-
