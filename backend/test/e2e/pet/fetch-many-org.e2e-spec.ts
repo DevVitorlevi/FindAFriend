d9a5a3dd31d302ea8cb2d9b1993cc9c78deb9db3
Vitor Levi <levivitor8@gmail.com>
2026-01-02 18:12:02 -0300

    test(e2e): add test for should be able to fetch only pets of the authenticated org and should not allow an org to fetch pets from other org

---
+++ backend/test/e2e/pet/fetch-many-org.e2e-spec.ts
@@ -0,0 +1,65 @@
+import { app } from '@/app.js'
+import request from 'supertest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { createAndAuthenticateOrg } from '../../utils/authenticate.js'
+import { createPet } from '../../utils/create-pet.js'
+import { resetDatabase } from '../../utils/reset-database.js'
+
+describe('Fetch Many Pet Of Org (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(async () => {
+    await resetDatabase()
+  })
+
+  it('should be able to fetch only pets of the authenticated org', async () => {
+    const { token, org } = await createAndAuthenticateOrg(app)
+    const { token: otherToken, org: otherOrg } =
+      await createAndAuthenticateOrg(app)
+
+    await createPet(app, {
+      orgId: org.id,
+      token,
+      name: 'Rex',
+    })
+
+    await createPet(app, {
+      orgId: org.id,
+      token,
+      name: 'Pingo',
+    })
+
+    await createPet(app, {
+      orgId: otherOrg.id,
+      token: otherToken,
+      name: 'Bolt',
+    })
+
+    const response = await request(app.server)
+      .get(`/pets/${org.id}`)
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.pets).toHaveLength(2)
+    response.body.pets.forEach((pet: any) => {
+      expect(pet.org_id).toBe(org.id)
+    })
+  })
+
+  it('should not allow an org to fetch pets from other org', async () => {
+    const { token } = await createAndAuthenticateOrg(app)
+    const { org: otherOrg } = await createAndAuthenticateOrg(app)
+
+    const response = await request(app.server)
+      .get(`/pets/${otherOrg.id}`)
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(403)
+  })
+})
