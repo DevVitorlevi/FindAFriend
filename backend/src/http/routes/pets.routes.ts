0bef177e11897730633764bc17706dfc541bc6c6
Vitor Levi <levivitor8@gmail.com>
2026-01-02 18:08:13 -0300

    feat(routes): add route to fetch many of org

--- backend/src/http/routes/pets.routes.ts
+++ backend/src/http/routes/pets.routes.ts
@@ -2,12 +2,14 @@ import type { FastifyInstance } from "fastify";
 import { adopted } from "../controllers/pets/adopted.js";
 import { create } from "../controllers/pets/create.js";
 import { deleteImage } from "../controllers/pets/delete-image.js";
+import { fetchManyOfOrg } from "../controllers/pets/fetch-many-org.js";
 import { fetchMany } from "../controllers/pets/fetch-many.js";
 import { getPet } from "../controllers/pets/get-pet.js";
 import { uploadImages } from "../controllers/pets/upload-images.js";
 import { verifyImageOwnership } from "../middlewares/verify-image-ownership.js";
 import { verifyJWT } from "../middlewares/verify-jwt.js";
 import { verifyPetOwnership } from "../middlewares/verify-pet-ownership.js";
+import { verifyOwnership } from "../middlewares/verify-ownership.js";
 
 export function petsRoutes(app: FastifyInstance) {
   app.post("/pets/:orgId/create", { onRequest: [verifyJWT] }, create)
@@ -16,4 +18,5 @@ export function petsRoutes(app: FastifyInstance) {
   app.post("/pet/:petId/images", { onRequest: [verifyJWT, verifyPetOwnership] }, uploadImages)
   app.patch("/pet/:petId", { onRequest: [verifyJWT, verifyPetOwnership] }, adopted)
   app.delete("/image/:imageId", { onRequest: [verifyJWT, verifyImageOwnership] }, deleteImage)
+  app.get("/pets/:orgId", { onRequest: [verifyJWT, verifyOwnership] }, fetchManyOfOrg)
 }
