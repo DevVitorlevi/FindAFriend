2ae3e513bd52d010e34ed7e697a7624e5a780c46
Vitor Levi <levivitor8@gmail.com>
2026-01-03 03:06:38 -0300

    clean

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,20 +3,22 @@ import cors from "@fastify/cors";
 import fastifyJwt from "@fastify/jwt";
 import multipart from '@fastify/multipart';
 import fastify from "fastify";
-export const app = fastify()
-
 import { env } from '@/env/index.js';
 import { v2 as cloudinary } from 'cloudinary';
 import z, { ZodError } from "zod";
 import { orgsRoutes } from "./http/routes/orgs.routes.js";
 import { petsRoutes } from "./http/routes/pets.routes.js";
 
+export const app = fastify()
+
 app.register(multipart)
+
 cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_API_SECRET,
 })
+
 app.register(cookie)
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -31,14 +33,14 @@ app.register(fastifyJwt, {
 
 app.register(cors, {
   origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 });
 
-//Routes
 app.register(orgsRoutes)
 app.register(petsRoutes)
-app.setErrorHandler((error, _, reply) => {
 
+app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
       message: 'Validation Error',
