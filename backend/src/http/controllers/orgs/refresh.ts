a8738a560bfbc8cc235320ad2a7949bedd6786ca
Vitor Levi <levivitor8@gmail.com>
2026-01-03 03:08:20 -0300

    feat(controller): add refresh token in reply

--- backend/src/http/controllers/orgs/refresh.ts
+++ backend/src/http/controllers/orgs/refresh.ts
@@ -3,35 +3,36 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true })
 
   const token = await reply.jwtSign(
+    {},
     {
-    },
-    {
-      sign: {
-        sub: request.user.sub,
-
-      }
+      sub: request.user.sub,
     }
   )
 
   const refreshToken = await reply.jwtSign(
+    {},
     {
-      sign: {
-        sub: request.user.sub,
-        expiresIn: "7d"
-
-      }
+      sub: request.user.sub,
+      expiresIn: "7d"
     }
   )
 
-  return reply.status(200).setCookie('refreshToken', refreshToken, {
-    path: '/',
-    secure: true,
-    sameSite: true,
-    httpOnly: true
-  }
-
-  ).send({
-    token
-  })
-
+  return reply.status(200)
+    .setCookie('accessToken', token, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 10
+    })
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 60 * 24 * 7
+    })
+    .send({
+      message: 'Tokens refreshed'
+    })
 }
