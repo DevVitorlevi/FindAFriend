525a6cae03fbc4b8a311847660fcb54c4d487807
Vitor Levi <levivitor8@gmail.com>
2026-01-03 03:14:30 -0300

    feat(routes): add routes for me

--- backend/src/http/routes/orgs.routes.ts
+++ backend/src/http/routes/orgs.routes.ts
@@ -1,10 +1,13 @@
 import type { FastifyInstance } from "fastify";
 import { login } from "../controllers/orgs/login.js";
+import { me } from "../controllers/orgs/me.js";
 import { refresh } from "../controllers/orgs/refresh.js";
 import { register } from "../controllers/orgs/register.js";
+import { verifyJWT } from "../middlewares/verify-jwt.js";
 
 export function orgsRoutes(app: FastifyInstance) {
   app.post("/orgs", register)
   app.post("/sessions", login)
+  app.get("/me", { onRequest: [verifyJWT] }, me)
   app.patch("/token/refresh", refresh)
 }
