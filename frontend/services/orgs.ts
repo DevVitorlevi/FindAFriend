eabf90e744fa3239667aeb0af86b728f3e99ae7e
Vitor Levi <levivitor8@gmail.com>
2026-01-03 03:17:25 -0300

    feat(service): add api call for get me

--- frontend/services/orgs.ts
+++ frontend/services/orgs.ts
@@ -10,17 +10,40 @@ export interface OrgRegisterRequest {
 }
 
 export interface OrgLoginRequest {
-  email: string,
+  email: string
   password: string
 }
 
+export interface Org {
+  id: string
+  name: string
+  email: string
+  state: string
+  city: string
+  whatsapp: string
+}
+
+export interface LoginResponse {
+  org: Org
+}
+
+export interface MeResponse {
+  org: Org
+}
 
 export async function registerOrg(data: OrgRegisterRequest) {
   const response = await petAPI.post("/orgs", data)
   return response.data
 }
 
-export async function loginOrg(data: OrgLoginRequest) {
+export async function loginOrg(
+  data: OrgLoginRequest
+): Promise<LoginResponse> {
   const response = await petAPI.post("/sessions", data)
+  return response.data
+}
+
+export async function getMe(): Promise<MeResponse> {
+  const response = await petAPI.get("/me")
   return response.data
 }
