dfb471eee4507752e81903288b19b5eb7455451b
Vitor Levi <levivitor8@gmail.com>
2026-01-08 15:36:33 -0300

    config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,60 +1,58 @@
 import cookie from "@fastify/cookie";
 import cors from "@fastify/cors";
 import fastifyJwt from "@fastify/jwt";
-import multipart from '@fastify/multipart';
+import multipart from "@fastify/multipart";
 import fastify from "fastify";
-import { env } from '@/env/index.js';
-import { v2 as cloudinary } from 'cloudinary';
+import { env } from "@/env/index.js";
+import { v2 as cloudinary } from "cloudinary";
 import z, { ZodError } from "zod";
 import { orgsRoutes } from "./http/routes/orgs.routes.js";
 import { petsRoutes } from "./http/routes/pets.routes.js";
 
-export const app = fastify()
+export const app = fastify();
 
-app.register(multipart)
+app.register(multipart);
 
 cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_API_SECRET,
-})
+});
 
-app.register(cookie)
+app.register(cookie);
+
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
-    cookieName: "refreshToken",
-    signed: false
+    cookieName: "accessToken",
+    signed: false,
   },
-  sign: {
-    expiresIn: "10m"
-  }
-})
+});
 
 app.register(cors, {
   origin: "http://localhost:3000",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 });
 
-app.register(orgsRoutes)
-app.register(petsRoutes)
+app.register(orgsRoutes);
+app.register(petsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
-      message: 'Validation Error',
-      issues: z.treeifyError(error)
-    })
+      message: "Validation Error",
+      issues: z.treeifyError(error),
+    });
   }
 
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   }
 
   return reply.status(500).send({
-    message: 'Internal Server Error'
-  })
-})
+    message: "Internal Server Error",
+  });
+});
 
 export { cloudinary };
