b8d24469d2b241f20a51586ade410d07bb67a59a
Vitor Levi <levivitor8@gmail.com>
2026-06-22 12:29:03 -0300

    config: improve unit test config

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,6 +1,10 @@
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    name: "unit",
+    include: ["test/unit/**/*.spec.ts"],
+  },
 });
